b55a8805ddb89b0c086a9b903f49896ddca6d01b
Weaver Goldman <we.goldm@gmail.com>
2022-06-29 19:03:09 -0400

    Added features for viewing in table format and converting to csv.

---
+++ cougar_log/log_helpers.py
@@ -0,0 +1,127 @@
+from datetime import datetime
+from pathlib import Path
+import pandas as pd
+
+from cougar_log.data_log_reader import DataLogReader
+
+HEADER_LIST = ["Timestamp", "Name", "Value"]
+
+
+def filter_dataframe(dataframe: pd.DataFrame, name_filter: str):
+    return dataframe.loc[dataframe[HEADER_LIST[1]] == name_filter]
+
+
+def read_log_to_dataframe(input_path: Path):
+    if input_path is None:
+        return [None, "No input file provided"]
+
+    if input_path.is_file():
+        # Verify the file format of the provided path
+        if input_path.suffix != ".wpilog":
+            return [None, "Invalid file format provided. Try providing a .wpilog file."]
+
+        [output, error] = convert_data_log_to_list(input_path=input_path)
+
+        if error is not None:
+            return [None, error]
+
+        # Convert the log data to a pandas dataframe
+        log_dataframe = pd.DataFrame(output)
+
+        log_dataframe.columns = HEADER_LIST
+
+        return [log_dataframe, error]
+
+    elif input_path.is_dir():
+        return [None, "The given input file is a directory."]
+    elif not input_path.exists():
+        return [None, "The input file doesn't exist"]
+
+
+def convert_data_log_to_list(input_path: str):
+    import mmap
+
+    output = []
+
+    error = None
+
+    error_message = "Invalid file, verify that the file is a wpilog or try downloading the log file again."
+
+    with open(input_path, "r") as f:
+        mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+        reader = DataLogReader(mm)
+
+        entries = {}
+
+        for record in reader:
+            # Store all record starting data in the entries dictionary
+            if record.isStart():
+                try:
+                    data = record.getStartData()
+                    entries[data.entry] = data
+                except TypeError as e:
+                    error = error_message
+
+            # Delete the finish entry of the current record
+            elif record.isFinish():
+                try:
+                    entry = record.getFinishEntry()
+                    if entry in entries:
+                        del entries[entry]
+                except TypeError as e:
+                    error = error_message
+
+            # Verify any available metadata
+            elif record.isSetMetadata():
+                try:
+                    data = record.getSetMetadataData()
+                except TypeError as e:
+                    error = error_message
+
+            # Verify that the type of the record is recognized
+            elif record.isControl():
+                error = error_message
+
+            # Extract and store the information from the current record
+            else:
+                entry = entries.get(record.entry, None)
+
+                if entry is None:
+                    continue
+
+                output.append(extract_value_from_entry(entry, record))
+
+    return [output, error]
+
+
+def extract_value_from_entry(entry, record):
+    timestamp = record.timestamp / 1000000
+
+    value = None
+
+    # Handle the system time-type entries
+    if entry.name == "systemTime" and entry.type == "int64":
+        dt = datetime.fromtimestamp(record.getInteger() / 1000000)
+        value = "{:%Y-%m-%d %H:%M:%S.%f}".format(dt)
+    else:
+        match entry.type:
+            case "double":
+                value = record.getDouble()
+            case "int64":
+                value = record.getInteger()
+            case "string" | "json":
+                value = record.getString()
+            case "boolean":
+                value = record.getBoolean()
+            case "boolean[]":
+                value = list(record.getBooleanArray())
+            case "double[]":
+                value = list(record.getDoubleArray())
+            case "float[]":
+                value = list(record.getFloatArray())
+            case "int64[]":
+                value = list(record.getIntegerArray())
+            case "string[]":
+                value = list(record.getStringArray())
+
+    return [timestamp, entry.name, value]
--- cougar_log/main.py
+++ cougar_log/main.py
@@ -1,9 +1,91 @@
-def fun():
-    print("hi")
+from pathlib import Path
 
-    somelongarray = [
-        "fhsdkfhsdfhjksdhfkjsdhfsdjkfhsdk",
-        "fsdjhfsdjkfhjksdfhsdhfjksdfhsdjhs",
-        "sfjsdklfjsdklfjlsdfjkjsdkfsdjj",
-        "flsdjlfksdfjljlksfklsdfklsdjfl",
-    ]
+import typer
+
+from cougar_log.log_helpers import HEADER_LIST, filter_dataframe, read_log_to_dataframe
+
+app = typer.Typer()
+
+
+def exit_with_error(error):
+    typer.echo(f"Error: {error}")
+    raise typer.Exit(code=1)
+
+
+@app.command()
+def convert(
+    input_path: Path = typer.Option(
+        None,
+        "--input",
+        "-i",
+        prompt="Enter the path to the file to convert",
+        help="The file provided to convert.",
+    ),
+    output_path: Path = typer.Option(
+        "output.csv",
+        "--output",
+        "-o",
+        help="The name of the resulting file.",
+    ),
+    name_filter: str = typer.Option(
+        None,
+        "--filter",
+        "-f",
+        help="Filter by a specific name in the logs.",
+    ),
+):
+    """
+    This command will convert a given wpilog file into a csv.
+
+    Optionally use filter to select only logs containing a given name.
+    """
+    [log_dataframe, error] = read_log_to_dataframe(input_path=input_path)
+
+    if error is not None:
+        exit_with_error(error)
+
+    if name_filter is not None:
+        log_dataframe = filter_dataframe(log_dataframe, name_filter=name_filter)
+
+    log_dataframe.to_csv(output_path, index=False)
+
+    typer.echo("Successfully converted and exported the provided file!")
+
+
+@app.command()
+def table(
+    input_path: Path = typer.Option(
+        None,
+        "--input",
+        "-i",
+        prompt="Enter the path to the file to tabulate",
+        help="The file provided to tabulate.",
+    ),
+    name_filter: str = typer.Option(
+        None,
+        "--filter",
+        "-f",
+        help="Filter by a specific name in the logs.",
+    ),
+):
+    """
+    This command will display the contents of a wpilog file in a table.
+
+    Optionally use filter to select only logs containing a given name.
+    """
+    [log_dataframe, error] = read_log_to_dataframe(input_path=input_path)
+
+    if error is not None:
+        exit_with_error(error)
+
+    if name_filter is not None:
+        log_dataframe = filter_dataframe(log_dataframe, name_filter=name_filter)
+
+    from tabulate import tabulate
+
+    typer.echo(tabulate(log_dataframe.set_index(HEADER_LIST[0]), headers=HEADER_LIST))
+
+
+@app.command()
+def visualize():
+    typer.echo("Random")
